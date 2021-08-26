7ecc555f11b5376d76d924db92ca0c47c8ea9441
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-08-25 22:53:00 -0400

    some styling

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "@chakra-ui/react";
+import { ActiveLink } from "../ActiveLink";
+import styles from "./navbar.module.scss";
 import {
   Box,
   Stack,
@@ -39,24 +42,35 @@ export function Menu() {
         mt={{ base: 4, md: 0 }}
         mr="auto"
         ml="auto"
+        className={styles.navbarComponent}
       >
-        <Text>Browse</Text>
-        <Text>Trending</Text>
-        <Text>Contribute</Text>
-        <Text>Community</Text>
+        <Text>
+          <ActiveLink href="/recipes" activeClassName={styles.active}>
+            <a href="/recipes/">Browse</a>
+          </ActiveLink>
+        </Text>
+        <Text>
+          <Link href="/trending/" activeClassName={styles.active}>
+            <a href="/recipes/">Trending </a>
+          </Link>
+        </Text>
+        <Text>
+          <Link href="/contribute/" activeClassName={styles.active}>
+            <a href="/contribute/">Contribute </a>
+          </Link>
+        </Text>
+        <Text>
+          <Link href="/community/" activeClassName={styles.active}>
+            <a href="/community/">Community </a>
+          </Link>
+        </Text>
       </Stack>
 
       <Box
         display={{ base: isOpen ? "block" : "none", md: "block" }}
         mt={{ base: 4, md: 0 }}
       >
-        <Button
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          outline="none"
-          variant="ghost"
-        >
+        <Button variant="outline">
           <span>Search</span>
           <SearchIcon ml={3} />
         </Button>
